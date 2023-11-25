d0e989f8ac3baf58aa0820e3045360c54d31a097
esraamurry7 <144175280+esraamurry7@users.noreply.github.com>
2023-11-24 22:21:29 -0600

    activity-5

---
+++ esraa-murry-2/blog/gatsby-config.js
@@ -0,0 +1,39 @@
+require('dotenv').config({
+    path: `.env.${process.env.NODE_ENV}`,
+  });
+  
+  module.exports = {
+    siteMetadata: {
+      title: 'SAROO2a CAFE',
+      description: 'ITDEV-164 Gatsby Blog.',
+      author: 'Esraa Murry',
+      siteUrl: 'https://gatsbystarterdefaultsource.gatsbyjs.io/',
+      contact: {
+        name: 'Esraa Murry',
+        company: 'Saroo2a Cafe',
+        address: '9572 west prairie grass way',
+      },
+    },
+    plugins: [
+      'gatsby-plugin-image',
+      'gatsby-plugin-styled-components',
+      {
+        resolve: 'gatsby-source-filesystem',
+        options: {
+          name: 'images',
+          path: `${__dirname}/src/images`,
+        },
+      },
+      {
+        resolve: 'gatsby-source-contentful',
+        options: {
+          spaceId: process.env.SPACE_ID,
+          accessToken: process.env.ACCESS_TOKEN,
+        }
+      },
+      'gatsby-transformer-remark',
+      'gatsby-transformer-sharp',
+      'gatsby-plugin-sharp',
+    ],
+  };
+  
